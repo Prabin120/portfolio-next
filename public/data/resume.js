36130ab6b05c40fafd93d5a4f6ccf0f01e9d42e0
Prabin120 <87239578+Prabin120@users.noreply.github.com>
2024-05-27 11:54:39 +0530

    resume link changes

--- public/data/resume.js
+++ public/data/resume.js
@@ -1,5 +1,5 @@
 const resume =
     {
-        resume: "images/1708968017888-Prabin-Resume.pdf", 
+        link: "/resume/Prabin-Resume.pdf", 
     }
 export default resume;
--- src/app/resume/page.jsx
+++ src/app/resume/page.jsx
@@ -1,31 +1,47 @@
-"use client"
+'use client'
 import { useEffect, useState } from "react";
 import resume from "../../../public/data/resume";
 
 export default function Resume() {
-    const [pdf,setPdf] = useState(null);
-    useEffect(()=>{
-        setPdf(resume)
-    },[])
-    const pdfURL = pdf ? URL+pdf.resume+"#page=1&zoom=100" : "";
+    const [pdf, setPdf] = useState(null);
 
-    function pdfDownloder(){
+    useEffect(() => {
+        setPdf(resume);
+    }, []);
+
+    const pdfURL = pdf ? `${pdf.link}#page=1&zoom=100` : "";
+    console.log(resume.link);
+    function pdfDownloader() {
         const link = document.createElement('a');
-        link.href = pdfURL;
+        link.href = pdf.link;
         link.target = "_blank";
         link.download = "Prabin_Resume.pdf";
-        const event = new MouseEvent('click');
-        link.dispatchEvent(event);
+        link.click();
     }
+
     return (
         <div className='min-h-screen'>
-            <h2 className={`text-center text-5xl pt-20 pb-5 font-mono max-[700px]:text-3xl`}>Resume <span className='text-yellow-500 font-bold'>View</span></h2>
+            <h2 className={`text-center text-5xl pt-20 pb-5 font-mono max-[700px]:text-3xl`}>
+                Resume <span className='text-yellow-500 font-bold'>View</span>
+            </h2>
             <div className="flex justify-center">
-                <button className="bg-slate-700 mb-4 rounded-lg px-5 py-2 hover:bg-yellow-600" onClick={pdfDownloder} type="button">Download</button>
+                <button
+                    className="bg-slate-700 mb-4 rounded-lg px-5 py-2 hover:bg-yellow-600"
+                    onClick={pdfDownloader}
+                    type="button"
+                >
+                    Download
+                </button>
             </div>
             <div className="flex justify-center">
-                <iframe src={`${pdfURL}&toolbar=0`} type="application/pdf" className="w-screen max-w-[75rem] h-screen"></iframe>
+                {pdfURL && (
+                    <iframe
+                        src={`${pdfURL}&toolbar=0`}
+                        type="application/pdf"
+                        className="w-screen max-w-[75rem] h-screen"
+                    ></iframe>
+                )}
             </div>
         </div>
-    )
+    );
 }
